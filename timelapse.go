71292441d2179dc8e448fdd53a004e8d3b56b974
Tim Haynes <github@stirfried.vegetable.org.uk>
2022-04-25 11:49:39 +0100

    Transform list of files into list of tasks to blend

--- timelapse.go
+++ timelapse.go
@@ -12,11 +12,20 @@ import (
 	exif "github.com/rwcarlsen/goexif/exif"
 )
 
+// tuple pair linking files and their mtime
 type TLIFile struct {
 	mtime int64
 	fname string
 }
 
+// tuple triple representing a blend task alpha proportion between files a and b
+type TLItask struct {
+	afile string
+	bfile string
+	alpha float64
+}
+
+// return image file's creation date (from EXIF if possible, else fs mtime)
 func getCtime(fname string) TLIFile {
 	fi, _ := os.Stat(fname)
 	var thing TLIFile
@@ -40,6 +49,50 @@ func getCtime(fname string) TLIFile {
 	return thing
 }
 
+// Convert list of files to list of tasks
+func files2tasks(data []TLIFile, noframes int64) []TLItask {
+	var ret []TLItask
+	var i int64
+	var task TLItask
+
+	nopoints := int64(len(data))
+	mint := float64(data[0].mtime)
+	maxt := float64(data[nopoints-1].mtime)
+
+	for i = 0; i <= noframes-1; i++ {
+		tstmp := mint + (maxt-mint)*float64(i)/float64(noframes-1)
+		var n int64
+		var m int64
+
+		alpha := float64(0)
+
+		for n = 0; n < nopoints-1 && float64(data[n].mtime) < tstmp; n++ {
+		}
+		n--
+		if n < 0 {
+			n = 0
+		}
+		for m = nopoints - 1; m > n+1 && float64(data[m].mtime) > tstmp; m-- {
+		}
+		if i == noframes-1 {
+			m = nopoints - 1
+			n = m
+		}
+		if m == n {
+			alpha = float64(0)
+		} else {
+			alpha = (tstmp - float64(data[n].mtime)) / float64(data[m].mtime-data[n].mtime)
+		}
+		fmt.Printf("i=%d, n=%d, m=%d, tstmp=%f, mtimes=[%d,%d], alpha=%f\n", i, n, m, tstmp, data[n].mtime, data[m].mtime, alpha)
+		task.afile = data[n].fname
+		task.bfile = data[m].fname
+		task.alpha = alpha
+		ret = append(ret, task)
+	}
+	return ret
+}
+
+// Generate list of image (jpg) files under given dir
 func findfiles(dir string) []string {
 	vals, _ := filepath.Glob(dir + "/*.jpg")
 	return vals
@@ -50,6 +103,7 @@ func main() {
 	framestr := os.Args[2]
 	outdir := os.Args[3]
 	frames, _ := strconv.Atoi(framestr)
+	noframes := int64(frames)
 	dirlist := findfiles(indir)
 	fmt.Printf("Would generate %d files into %s\n", frames, outdir)
 	fmt.Println("Found files: ", dirlist)
@@ -59,4 +113,7 @@ func main() {
 		return moredata[i].mtime < moredata[j].mtime
 	})
 	fmt.Println(moredata)
+	tasks := files2tasks(moredata, noframes)
+	fmt.Println("Task list")
+	fmt.Println(tasks)
 }
